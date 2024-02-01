c2d6938cc4d13e3933cbe8c8f058c1c9e566154d
Felipe Aliaga <faliaga@siroe.cl>
2024-02-01 11:27:55 -0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
+import { DetalleProductoComponent } from './components/detalle-producto/detalle-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductosComponent,
+    DetalleProductoComponent
   ],
   imports: [
     BrowserModule

2e008d91aa47a5a9badd4c686e398755036f107f
Felipe Aliaga <faliaga@siroe.cl>
2024-02-02 15:45:56 -0300

    fix

--- src/app/components/caja-supermercado/caja-supermercado.component.ts
+++ src/app/components/caja-supermercado/caja-supermercado.component.ts
@@ -11,14 +11,12 @@ export class CajaSupermercadoComponent {
   codigoBarras: string = '';
   productosAgregados: any[] = [];
   totalCompra: number = 0;
-  dineroRecibido: string = '' ;
+  dineroRecibido: string = '';
   vuelto: number = 0;
   mostrarVistaDetalle: boolean = false;
-
   respuestaProductos: any[] = [];
 
   constructor(private productService: ProductService) { }
-
 
   ngOnInit() {
     this.productService.getAllProducts().subscribe(
@@ -33,22 +31,17 @@ export class CajaSupermercadoComponent {
 
   agregarProductoPorCodigo() {
     if (this.codigoBarras) {
-      // Realiza la búsqueda del producto por código de barras en la respuesta JSON
       const producto = this.respuestaProductos.find((p) => p.codigo_barra === this.codigoBarras);
-
       if (producto) {
-        // Agrega el producto a la lista de productos agregados
         this.productosAgregados.push(producto);
-
-        // Incrementa el total de la compra
         this.totalCompra += producto.valor;
-
-        // Limpia el campo de texto después de agregar el producto
         this.codigoBarras = '';
 
-        
+        // Verifica si ya se ingresó un monto en dineroRecibido antes de calcular el vuelto
+        if (this.dineroRecibido) {
+          this.calcularVuelto();
+        }
       } else {
-        // Muestra un mensaje de error si el código de barras no corresponde a un producto válido.
         Swal.fire({
           icon: 'error',
           title: 'Error',
@@ -57,7 +50,6 @@ export class CajaSupermercadoComponent {
         this.codigoBarras = '';
       }
     } else {
-      // Muestra un mensaje de error si el campo de código de barras está vacío.
       Swal.fire({
         icon: 'error',
         title: 'Campo Vacío',
@@ -66,30 +58,27 @@ export class CajaSupermercadoComponent {
     }
   }
 
-
   quitarProducto(indice: number) {
     const producto = this.productosAgregados[indice];
     if (producto) {
-      // Asegurándose de que el valor a restar es un número
-      const precioProducto = Number(producto.valor); // Cambia 'valor' a 'precio' si es necesario
-
+      const precioProducto = Number(producto.valor);
       if (!isNaN(precioProducto)) {
-        // Restar el valor del producto del total de la compra
         this.totalCompra -= precioProducto;
+        this.productosAgregados.splice(indice, 1);
+
+        // Verifica si ya se ingresó un monto en dineroRecibido antes de calcular el vuelto
+        if (this.dineroRecibido) {
+          this.calcularVuelto();
+        }
       } else {
         console.error('El valor del producto no es un número');
       }
-
-      // Quitar el producto de la lista de productos agregados
-      this.productosAgregados.splice(indice, 1);
     }
 
-    // Verificar si la lista de productos está vacía y establecer el total en 0 si es así
     if (this.productosAgregados.length === 0) {
       this.totalCompra = 0;
     }
   }
-
 
   limpiarCarrito() {
     this.productosAgregados = [];
@@ -101,7 +90,6 @@ export class CajaSupermercadoComponent {
     this.mostrarVistaDetalle = true; // Mostrar la vista nueva
   }
 
-  
   calcularVuelto() {
     // Convierte dineroRecibido a número
     const dineroRecibidoNumero = parseFloat(this.dineroRecibido);
@@ -123,6 +111,7 @@ export class CajaSupermercadoComponent {
         text: 'El dinero recibido es insuficiente para pagar la compra.',
       });
       this.vuelto = 0;
+      this.dineroRecibido = '';
     }
   }
 
@@ -135,5 +124,4 @@ export class CajaSupermercadoComponent {
     this.mostrarVistaDetalle = false;
     // Aquí puedes agregar cualquier otra lógica necesaria para finalizar la compra
   }
-
 }

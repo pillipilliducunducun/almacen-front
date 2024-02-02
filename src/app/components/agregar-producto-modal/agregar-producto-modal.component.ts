b63b2d96b52b3d23c5da726edc7a31bfd8aabe58
Felipe Aliaga <faliaga@siroe.cl>
2024-02-01 21:04:28 -0300

    fix

--- src/app/components/agregar-producto-modal/agregar-producto-modal.component.ts
+++ src/app/components/agregar-producto-modal/agregar-producto-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-agregar-producto-modal',
@@ -6,15 +7,84 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./agregar-producto-modal.component.css']
 })
 export class AgregarProductoModalComponent {
-  // EventEmitter para notificar al componente padre que el modal debe cerrarse
+  @Output() refrescarProductos = new EventEmitter<void>();
   @Output() cerrarModal = new EventEmitter<void>();
+  @Input() set productoActual(value: any) {
+    if (value) {
+      this.idProducto = value.id;  // Asegúrate de que el producto tiene un campo 'id'
+      this.nombreProducto = value.nombre;
+      this.codigoBarras = value.codigo_barra;
+      this.precioProducto = value.valor;
+      this.esModoEdicion = true;
+    } else {
+      this.resetForm();
+      this.esModoEdicion = false;
+    }
+  }
 
-  constructor() { }
 
-  // Método para cerrar el modal, emite el evento para notificar al componente padre
+  nombreProducto: string = '';
+  codigoBarras: string = '';
+  precioProducto: number | null = null;
+  esModoEdicion: boolean = false;
+  idProducto: number | null = null;
+
+  
+
+  constructor(private productService: ProductService) { }
+
   cerrar() {
     this.cerrarModal.emit();
   }
 
-  // Aquí puedes agregar más lógica, como la función para agregar un nuevo producto, validaciones, etc.
+  guardarProducto() {
+    if (!this.nombreProducto || !this.codigoBarras || this.precioProducto === null) {
+      console.error('Todos los campos son necesarios');
+      return;
+    }
+
+    const producto = {
+      nombre: this.nombreProducto,
+      codigo_barra: this.codigoBarras,
+      valor: this.precioProducto
+    };
+
+    if (this.esModoEdicion) {
+      if (this.idProducto === null) {
+        console.error('Error: No se proporcionó un ID de producto para la actualización.');
+        return;
+      }
+
+      this.productService.updateProduct(this.idProducto, producto).subscribe({
+        next: (response) => {
+          console.log('Producto actualizado', response);
+          this.cerrar();
+          this.refrescarProductos.emit();
+        },
+        error: (error) => {
+          console.error('Error al actualizar el producto', error);
+        }
+      });
+    } else {
+      this.productService.createProduct(producto).subscribe({
+        next: (response) => {
+          console.log('Producto agregado', response);
+          this.cerrar();
+          this.refrescarProductos.emit();
+        },
+        error: (error) => {
+          console.error('Error al agregar el producto', error);
+        }
+      });
+    }
+  }
+
+
+
+
+  resetForm() {
+    this.nombreProducto = '';
+    this.codigoBarras = '';
+    this.precioProducto = null;
+  }
 }
--- src/app/components/lista-productos/lista-productos.component.ts
+++ src/app/components/lista-productos/lista-productos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { AgregarProductoModalComponent } from '../agregar-producto-modal/agregar-producto-modal.component';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -14,15 +15,16 @@ export class ListaProductosComponent implements OnInit {
   nombreProducto: string = ''; // Propiedad para el nombre del producto
   productos: any[] = [ /* Aquí debes tener una lista de productos */];
   productosFiltrados: any[] = []; // Propiedad para los productos filtrados
+  precioProducto: number | null = null;
   mostrarModal: boolean = false;
+  productoActualParaEditar: any = null;
+
 
 
   constructor(private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.getAllProducts().subscribe(data => {
-      this.productos = data;
-    });
+    this.refrescarListaProductos();
   }
   // Función para buscar por código de barras
   buscarPorCodigo(event?: any) {
@@ -38,8 +40,46 @@ export class ListaProductosComponent implements OnInit {
     // Filtra los productos y actualiza productosFiltrados
   }
 
-  agregarProducto() {
+  eliminarProducto(productId: number) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "¡No podrás revertir esto!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, ¡elimínalo!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.productService.deleteProduct(productId).subscribe(() => {
+          Swal.fire(
+            '¡Eliminado!',
+            'El producto ha sido eliminado.',
+            'success'
+          );
+          this.refrescarListaProductos();
+          // Aquí deberías actualizar tu lista de productos, tal vez volviendo a cargarlos
+        });
+      }
+    });
+  }
+
+  // In ListaProductosComponent class
+
+  editarProducto(producto: any) {
+    this.productoActualParaEditar = producto;  // Asegúrate de que 'producto' incluya el 'id'
     this.mostrarModal = true;
+  }
+
+  agregarProducto() {
+    this.productoActualParaEditar = null;  // Ensure no product is set for editing
+    this.mostrarModal = true;
+  }
+
+  refrescarListaProductos() {
+    this.productService.getAllProducts().subscribe(data => {
+      this.productos = data;
+    });
   }
 
 
